8d5d8a7950f13797e7d0a1db373a6789e87ab366
Hyde Sun <hyde_sun@sina.com>
2019-05-05 21:10:06 +0800

    Add base template and error template

--- app.py
+++ app.py
@@ -4,7 +4,6 @@ import click
 from flask import Flask, render_template
 from flask_sqlalchemy import SQLAlchemy
 
-
 WIN = sys.platform.startswith('win')
 if WIN:
     prefix = 'sqlite:///'
@@ -12,24 +11,41 @@ else:
     prefix = 'sqlite:////'
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = prefix + os.path.join(app.root_path, 'data.db')
+app.config['SQLALCHEMY_DATABASE_URI'] = prefix + os.path.join(
+    app.root_path, 'data.db')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False  # 关闭对模型修改的监控
 db = SQLAlchemy(app)
+
 
 @app.route('/')
 def index():
     user = User.query.first()
     movies = Movie.query.all()
-    return render_template('index.html', user=user, movies=movies)
+    return render_template('index.html', movies=movies)
+
+
+@app.errorhandler(404)  # 传入要处理的错误代码
+def page_not_found(e):  # 接受异常对象作为参数
+    user = User.query.first()
+    return render_template('404.html'), 404  # 返回模板和状态码
+
+
+@app.context_processor
+def inject_user():
+    user = User.query.first()
+    return dict(user=user)
+
 
 class User(db.Model):
-    id = db.Column(db.Integer, primary_key = True)
+    id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(20))
 
+
 class Movie(db.Model):
-    id = db.Column(db.Integer, primary_key = True)
+    id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(60))
     year = db.Column(db.String(40))
+
 
 @app.cli.command()
 @click.option('--drop', is_flag=True, help='Create after drop.')  # 设置选项
@@ -40,6 +56,7 @@ def initdb(drop):
     db.create_all()
     click.echo('Initialized database.')  # 输出提示信息
 
+
 @app.cli.command()
 def forge():
     """Generate fake data."""
@@ -48,16 +65,46 @@ def forge():
     # 全局的两个变量移动到这个函数内
     name = 'Hyde Sun'
     movies = [
-        {'title': 'My Neighbor Totoro', 'year': '1988'},
-        {'title': 'Dead Poets Society', 'year': '1989'},
-        {'title': 'A Perfect World', 'year': '1993'},
-        {'title': 'Leon', 'year': '1994'},
-        {'title': 'Mahjong', 'year': '1996'},
-        {'title': 'Swallowtail Butterfly', 'year': '1996'},
-        {'title': 'King of Comedy', 'year': '1999'},
-        {'title': 'Devils on the Doorstep', 'year': '1999'},
-        {'title': 'WALL-E', 'year': '2008'},
-        {'title': 'The Pork of Music', 'year': '2012'},
+        {
+            'title': 'My Neighbor Totoro',
+            'year': '1988'
+        },
+        {
+            'title': 'Dead Poets Society',
+            'year': '1989'
+        },
+        {
+            'title': 'A Perfect World',
+            'year': '1993'
+        },
+        {
+            'title': 'Leon',
+            'year': '1994'
+        },
+        {
+            'title': 'Mahjong',
+            'year': '1996'
+        },
+        {
+            'title': 'Swallowtail Butterfly',
+            'year': '1996'
+        },
+        {
+            'title': 'King of Comedy',
+            'year': '1999'
+        },
+        {
+            'title': 'Devils on the Doorstep',
+            'year': '1999'
+        },
+        {
+            'title': 'WALL-E',
+            'year': '2008'
+        },
+        {
+            'title': 'The Pork of Music',
+            'year': '2012'
+        },
     ]
 
     user = User(name=name)
